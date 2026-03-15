6f6034d9dda887f61bcafde38ff3e6166a3024c5
Markus Stockhausen <markus.stockhausen@gmx.de>
2026-03-15 18:52:22 +0100

    realtek: dsa: move cpu_port to config structure
    The cpu_port is a per-device constant. Thus move it from the
    instance structure to the config structure.
    
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

--- target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/common.c
+++ target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/common.c
@@ -25,7 +25,7 @@ int rtldsa_port_get_stp_state(struct rtl838x_switch_priv *priv, int port)
 	u32 msti = 0;
 	int state;
 
-	if (port >= priv->cpu_port)
+	if (port >= priv->r->cpu_port)
 		return -EINVAL;
 
 	mutex_lock(&priv->reg_mutex);
@@ -299,7 +299,7 @@ static int rtl83xx_mdio_probe(struct rtl838x_switch_priv *priv)
 
 		pcs_node = of_parse_phandle(dn, "pcs-handle", 0);
 		phy_node = of_parse_phandle(dn, "phy-handle", 0);
-		if (pn != priv->cpu_port && !phy_node && !pcs_node) {
+		if (pn != priv->r->cpu_port && !phy_node && !pcs_node) {
 			dev_err(priv->dev, "Port node %d has neither pcs-handle nor phy-handle\n", pn);
 			continue;
 		}
@@ -839,7 +839,7 @@ int rtl83xx_port_is_under(const struct net_device *dev, struct rtl838x_switch_pr
 	 * }
 	 */
 
-	for (int i = 0; i < priv->cpu_port; i++) {
+	for (int i = 0; i < priv->r->cpu_port; i++) {
 		if (!priv->ports[i].dp)
 			continue;
 		if (priv->ports[i].dp->user == dev)
@@ -1546,7 +1546,7 @@ static irqreturn_t rtldsa_switch_irq(int irq, void *dev_id)
 	link = priv->r->get_port_reg_le(priv->r->mac_link_sts);
 	link = priv->r->get_port_reg_le(priv->r->mac_link_sts);
 
-	for (int port = 0; port < priv->cpu_port; port++)
+	for (int port = 0; port < priv->r->cpu_port; port++)
 		if (ports & BIT_ULL(port))
 			dsa_port_phylink_mac_change(ds, port, link & BIT_ULL(port));
 
@@ -1635,7 +1635,6 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 	switch (soc_info.family) {
 	case RTL8380_FAMILY_ID:
 		priv->ds->ops = &rtldsa_83xx_switch_ops;
-		priv->cpu_port = RTL838X_CPU_PORT;
 		priv->port_mask = 0x1f;
 		priv->port_width = 1;
 		priv->fib_entries = 8192;
@@ -1645,7 +1644,6 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL8390_FAMILY_ID:
 		priv->ds->ops = &rtldsa_83xx_switch_ops;
-		priv->cpu_port = RTL839X_CPU_PORT;
 		priv->port_mask = 0x3f;
 		priv->port_width = 2;
 		priv->fib_entries = 16384;
@@ -1655,7 +1653,6 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL9300_FAMILY_ID:
 		priv->ds->ops = &rtldsa_93xx_switch_ops;
-		priv->cpu_port = RTL930X_CPU_PORT;
 		priv->port_mask = 0x1f;
 		priv->port_width = 1;
 		priv->fib_entries = 16384;
@@ -1666,7 +1663,6 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL9310_FAMILY_ID:
 		priv->ds->ops = &rtldsa_93xx_switch_ops;
-		priv->cpu_port = RTL931X_CPU_PORT;
 		priv->port_mask = 0x3f;
 		priv->port_width = 2;
 		priv->fib_entries = 16384;
@@ -1676,8 +1672,8 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		priv->n_mst = 128;
 		break;
 	}
-	priv->ds->num_ports = priv->cpu_port + 1;
-	priv->irq_mask = GENMASK_ULL(priv->cpu_port - 1, 0);
+	priv->ds->num_ports = priv->r->cpu_port + 1;
+	priv->irq_mask = GENMASK_ULL(priv->r->cpu_port - 1, 0);
 
 	err = rtl83xx_mdio_probe(priv);
 	if (err) {
@@ -1709,7 +1705,7 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 	 * dsa_switch_tree, the tree is built when the switch
 	 * is registered by dsa_register_switch
 	 */
-	for (int i = 0; i <= priv->cpu_port; i++)
+	for (int i = 0; i <= priv->r->cpu_port; i++)
 		priv->ports[i].dp = dsa_to_port(priv->ds, i);
 
 	/* Enable link and media change interrupts. Are the SERDES masks needed? */

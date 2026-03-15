fec055a88844f35b8dc09b4f091f93d11c077630
Markus Stockhausen <markus.stockhausen@gmx.de>
2026-03-15 19:11:36 +0100

    realtek: dsa: drop port_width from instance structure
    The port_width attribute is only used once and can be derived
    from the cpu_port. Drop it.
    
    Signed-off-by: Markus Stockhausen <markus.stockhausen@gmx.de>

--- target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
+++ target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/dsa.c
@@ -1449,10 +1449,10 @@ static void rtldsa_port_xstp_state_set(struct rtl838x_switch_priv *priv, int por
 				       u8 state, u16 mst_slot)
 				       __must_hold(&priv->reg_mutex)
 {
+	int n = priv->r->cpu_port < 32 ? 2 : 4;
 	u32 port_state[4];
 	int index, bit;
 	int pos = port;
-	int n = priv->port_width << 1;
 
 	/* Ports above or equal CPU port can never be configured */
 	if (port >= priv->r->cpu_port)
